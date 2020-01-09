cb1f0aeca6b145c13ad0889014a95b03440e8b62
Elizabeth <elizabethsong65@yahoo.com>
2020-01-09 15:22:22 -0500

    added Nessie

--- app/src/main/java/com/example/goaldigger/MainActivity.java
+++ app/src/main/java/com/example/goaldigger/MainActivity.java
@@ -1,39 +1,68 @@
 package com.example.goaldigger;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.os.Bundle;
 
-import com.example.goaldigger.ui.main.SectionsPagerAdapter;
+import com.reimaginebanking.api.nessieandroidsdk.NessieResultsListener;
+import com.reimaginebanking.api.nessieandroidsdk.constants.TransactionMedium;
+import com.reimaginebanking.api.nessieandroidsdk.NessieError;
+import com.reimaginebanking.api.nessieandroidsdk.models.Customer;
+import com.reimaginebanking.api.nessieandroidsdk.models.Deposit;
+import com.reimaginebanking.api.nessieandroidsdk.models.PostResponse;
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+import android.util.Log;
+
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    NessieClient client = NessieClient.getInstance("4db704c8f8b013300a4a960683da8399");
+    Deposit deposit = new Deposit.Builder()
+            .amount(100)
+            .description("DEPOSIT")
+            .transactionDate("2016-08-09")
+            .medium(TransactionMedium.BALANCE)
+            .build();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-//        FloatingActionButton fab = findViewById(R.id.fab);
 
-//        fab.setOnClickListener(new View.OnClickListener() {
+
+        client.CUSTOMER.getCustomers(new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                List<Customer> customers = (List<Customer>) result;
+                // do something with the list of customers here
+            }
+
+            @Override
+            public void onFailure(NessieError error) {
+                // handle error
+                Log.e("Error", error.toString());
+            }
+        });
+
+//        client.DEPOSIT.createDeposit("deposit_id", deposit, new NessieResultsListener() {
 //            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+//            public void onSuccess(Object result) {
+//                PostResponse<Deposit> response = (PostResponse<Deposit>) result;
+//                Deposit deposit = response.getObjectCreated();
+//                // do something with the newly created deposit
 //            }
 //        });
+
+
+
     }
+
+
+
 }
+
+
